63e198c3bd6f91e825b0c0e2306558de5703dca8
Stoyan Stoyanov <95132005+Stoyan83@users.noreply.github.com>
2023-09-15 10:59:39 +0300

    Update test.rb

--- test.rb
+++ test.rb
@@ -7,30 +7,21 @@ on:
 jobs:
   test:
     runs-on: ubuntu-latest
-    services:
-      # Use an official SQLite image
-      sqlite:
-        image: sqlite:latest
-        ports:
-          - "5432:5432"
-        options: --health-cmd "echo 'SELECT 1;'" --health-interval 10s --health-timeout 5s --health-retries 5
     env:
       RAILS_ENV: test
-      DATABASE_URL: "sqlite3::memory:" # Use an in-memory SQLite database
     steps:
       - name: Checkout code
         uses: actions/checkout@v2
-      # Add or replace dependency steps here
       - name: Install Ruby and gems
-        uses: ruby/setup-ruby@v1
+        uses: ruby/setup-ruby@v1 # Use the latest version
         with:
           bundler-cache: true
-      # Add or replace database setup steps here
-      - name: Set up database schema
-        run: bin/rails db:schema:load
-      # Add or replace test runners here
+      - name: Set up database
+        run: |
+          bundle exec rails db:prepare
+          bundle exec rails db:migrate
       - name: Run tests
-        run: bin/rake
+        run: bundle exec rails test
 
   lint:
     runs-on: ubuntu-latest
@@ -38,13 +29,13 @@ jobs:
       - name: Checkout code
         uses: actions/checkout@v2
       - name: Install Ruby and gems
-        uses: ruby/setup-ruby@v1
+        uses: ruby/setup-ruby@v1 # Use the latest version
         with:
           bundler-cache: true
-      # Add or replace any other lints here
       - name: Security audit dependencies
-        run: bin/bundler-audit --update
+        run: bundle exec bundler-audit --update
       - name: Security audit application code
-        run: bin/brakeman -q -w2
+        run: bundle exec brakeman -q -w2
       - name: Lint Ruby files
-        run: bin/rubocop --parallel
+        run: bundle exec rubocop --parallel
+
